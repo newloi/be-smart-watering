c95e3cd1350061e76933fbbbf258fbae9952343f
newloi <luungocloi24052004@gmail.com>
2025-10-03 15:34:57 +0700

    init

---
+++ src/main/java/com/smart_watering_system/SmartWateringSystem/SmartWateringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.smart_watering_system.SmartWateringSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWateringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWateringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart_watering_system/SmartWateringSystem/SmartWateringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_watering_system.SmartWateringSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWateringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
