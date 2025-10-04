1546cbbe337b3f275030403111d82c6d26a72e30
newloi <luungocloi24052004@gmail.com>
2025-10-04 21:17:01 +0700

    config: properties in application.yaml and cors

---
+++ src/main/java/com/smart_watering_system/SmartWateringSystem/configuration/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.smart_watering_system.SmartWateringSystem.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+
+//enable authorization by annotation (e.g: @PreAuthorize("hasRole('ADMIN')"), ..)
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+
+
+
+}
