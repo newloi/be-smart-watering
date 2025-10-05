a022eb1c13c483c3490bc16128fda4cb87a9e2a2
newloi <luungocloi24052004@gmail.com>
2025-10-05 16:44:18 +0700

    config: password encoder

--- src/main/java/com/smart_watering_system/SmartWateringSystem/configuration/SecurityConfig.java
+++ src/main/java/com/smart_watering_system/SmartWateringSystem/configuration/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.smart_watering_system.SmartWateringSystem.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -27,6 +29,7 @@ public class SecurityConfig {
         return new CorsFilter(urlBasedCorsConfigurationSource);
     }
 
-
+    @Bean
+    PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(10); }
 
 }
