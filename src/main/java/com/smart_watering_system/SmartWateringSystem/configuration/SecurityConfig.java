acbb8d42708a3822e306a0b591f898ae54a7dd97
newloi <luungocloi24052004@gmail.com>
2025-10-06 14:49:59 +0700

    config: security filter chain

--- src/main/java/com/smart_watering_system/SmartWateringSystem/configuration/SecurityConfig.java
+++ src/main/java/com/smart_watering_system/SmartWateringSystem/configuration/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.smart_watering_system.SmartWateringSystem.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -14,6 +18,10 @@ import org.springframework.web.filter.CorsFilter;
 //enable authorization by annotation (e.g: @PreAuthorize("hasRole('ADMIN')"), ..)
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    private final String[] PUBLIC_ENDPOINTS = {
+      "/auth/log-in"
+    };
 
     @Bean
     public CorsFilter corsFilter() {
@@ -31,5 +39,16 @@ public class SecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(10); }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(request ->
+                request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
+                        .anyRequest().authenticated());
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
 
 }
