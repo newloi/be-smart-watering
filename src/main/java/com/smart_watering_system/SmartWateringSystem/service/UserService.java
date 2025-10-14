930e0b4a307f986dabf2a31d9669d569cee7790e
newloi <luungocloi24052004@gmail.com>
2025-10-14 21:16:56 +0700

    feat: job clean invalid token at 00:00 everyday

--- src/main/java/com/smart_watering_system/SmartWateringSystem/service/UserService.java
+++ src/main/java/com/smart_watering_system/SmartWateringSystem/service/UserService.java
@@ -3,20 +3,27 @@ package com.smart_watering_system.SmartWateringSystem.service;
 import com.nimbusds.jwt.SignedJWT;
 import com.smart_watering_system.SmartWateringSystem.dto.request.UserRequest;
 import com.smart_watering_system.SmartWateringSystem.dto.response.UserResponse;
+import com.smart_watering_system.SmartWateringSystem.entity.InvalidatedToken;
 import com.smart_watering_system.SmartWateringSystem.entity.User;
 import com.smart_watering_system.SmartWateringSystem.enums.ErrorCode;
 import com.smart_watering_system.SmartWateringSystem.exception.AppException;
 import com.smart_watering_system.SmartWateringSystem.mapper.UserMapper;
+import com.smart_watering_system.SmartWateringSystem.repository.InvalidatedTokenRepository;
 import com.smart_watering_system.SmartWateringSystem.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -25,6 +32,7 @@ public class UserService {
     UserRepository userRepository;
     UserMapper userMapper;
     PasswordEncoder passwordEncoder;
+    InvalidatedTokenRepository invalidatedTokenRepository;
 
     public UserResponse create(UserRequest request) {
         var user = userMapper.toUser(request);
@@ -47,6 +55,13 @@ public class UserService {
 
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void cleanInvalidateTokenTable() {
+        List<InvalidatedToken> invalidatedTokens = invalidatedTokenRepository.findByExpiryTimeBefore(new Date());
+
+        invalidatedTokenRepository.deleteAll(invalidatedTokens);
     }
 
 }
