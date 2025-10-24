2ecf99ff602f7d18b50c2688c297d9019321a9f7
newloi <luungocloi24052004@gmail.com>
2025-10-24 15:08:39 +0700

    refactor: use thymeleaf template engine for mail service

--- src/main/java/com/smart_watering_system/SmartWateringSystem/service/MailService.java
+++ src/main/java/com/smart_watering_system/SmartWateringSystem/service/MailService.java
@@ -11,6 +11,8 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -23,20 +25,21 @@ public class MailService {
     @NonFinal
     @Value("${spring.mail.username}")
     String srcEmail;
+
     JavaMailSender mailSender;
+    SpringTemplateEngine templateEngine;
 
-    public void sendOtpEmail(String desEmail, String otpCode) throws IOException, MessagingException {
-        ClassPathResource resource = new ClassPathResource("templates/otp-template.html");
-        String htmlContent = Files.readString(resource.getFile().toPath());
+    public void sendOtpEmail(String desEmail, String otpCode) throws MessagingException {
+        Context context = new Context();
+        context.setVariable("otpCode", otpCode);
 
-        htmlContent = htmlContent.replace("${otp}", otpCode);
+        String htmlContent = templateEngine.process("otp-template", context);
 
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
-
         helper.setFrom(srcEmail);
         helper.setTo(desEmail);
-        helper.setSubject("Smart Watering - Mã xác thực OTP của bạn");
+        helper.setSubject("Smart Watering - Mã xác thực OTP");
         helper.setText(htmlContent, true);
 
         mailSender.send(message);
