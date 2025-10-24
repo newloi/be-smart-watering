1cb1d0982331f288861639ed309ba9e6e0ba6c88
newloi <luungocloi24052004@gmail.com>
2025-10-24 23:54:36 +0700

    feat: enable caching with Caffeine

--- src/main/java/com/smart_watering_system/SmartWateringSystem/SmartWateringSystemApplication.java
+++ src/main/java/com/smart_watering_system/SmartWateringSystem/SmartWateringSystemApplication.java
@@ -2,12 +2,14 @@ package com.smart_watering_system.SmartWateringSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaAuditing
+@EnableCaching
 public class SmartWateringSystemApplication {
 
 	public static void main(String[] args) {
