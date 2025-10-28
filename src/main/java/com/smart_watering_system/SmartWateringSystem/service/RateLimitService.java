89ee19390b35e21077a48faa229d741bdd8405c6
newloi <luungocloi24052004@gmail.com>
2025-10-28 22:32:45 +0700

    refactor: using Caffein cache instead of using Map to caching

--- src/main/java/com/smart_watering_system/SmartWateringSystem/service/RateLimitService.java
+++ src/main/java/com/smart_watering_system/SmartWateringSystem/service/RateLimitService.java
@@ -1,29 +1,30 @@
 package com.smart_watering_system.SmartWateringSystem.service;
 
-import io.github.bucket4j.Bandwidth;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Refill;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class RateLimitService {
 
-    Map<String, Bucket> cache = new ConcurrentHashMap<>();
+    Cache<String, Bucket> cache = Caffeine.newBuilder()
+            .expireAfterAccess(Duration.ofMinutes(10))
+            .maximumSize(1000)
+            .build();
 
     public Bucket resolveBucket(
             String username, String methodKey, int capacity, int refillTokens, long refillPeriodSeconds) {
         String key = username + ":" + methodKey;
 
-        return cache.computeIfAbsent(key, k ->
+        return cache.get(key, k ->
                 Bucket.builder()
                         .addLimit(limit -> limit
                                 .capacity(capacity)
