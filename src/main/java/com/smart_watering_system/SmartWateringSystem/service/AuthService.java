f595bdb38eeac6771714fc5e9d79c37076b01a23
newloi <luungocloi24052004@gmail.com>
2025-11-13 14:48:51 +0700

    feat: remove require verify email when login

--- src/main/java/com/smart_watering_system/SmartWateringSystem/service/AuthService.java
+++ src/main/java/com/smart_watering_system/SmartWateringSystem/service/AuthService.java
@@ -45,7 +45,7 @@ public class AuthService {
         var user = userRepository.findByUsername(request.getUsername())
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
 
-        if (!user.isVerified()) throw new AppException(ErrorCode.ACC_NOT_VERIFIED);
+//        if (!user.isVerified()) throw new AppException(ErrorCode.ACC_NOT_VERIFIED);
 
         boolean isAuthenticated = passwordEncoder.matches(request.getPassword(), user.getPassword());
         if (!isAuthenticated) throw new AppException(ErrorCode.WRONG_PASSWORD);
