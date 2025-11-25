cb650e78ef6eaf94b9271fa75b2b1368cb403f6a
newloi <luungocloi24052004@gmail.com>
2025-11-25 20:09:31 +0700

    fix: map isVerified in User to isVerified in UserResponse

--- src/main/java/com/smart_watering_system/SmartWateringSystem/mapper/UserMapper.java
+++ src/main/java/com/smart_watering_system/SmartWateringSystem/mapper/UserMapper.java
@@ -4,9 +4,11 @@ import com.smart_watering_system.SmartWateringSystem.dto.request.UserRequest;
 import com.smart_watering_system.SmartWateringSystem.dto.response.UserResponse;
 import com.smart_watering_system.SmartWateringSystem.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User toUser(UserRequest request);
+    @Mapping(target = "isVerified", source = "verified")
     UserResponse toUserResponse(User user);
 }
